81194e12bce12e154695fe7d4891022a99643a1c
abhinavyadav <abnvv19@gmail.com>
2026-09-01 13:42:21 +0530

    fixed some security bugs

--- src/main/java/in/abnvv/new2Api/SpringCrudAPI.java
+++ src/main/java/in/abnvv/new2Api/SpringCrudAPI.java
@@ -2,13 +2,12 @@ package in.abnvv.new2Api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-public class New2ApiApplication {
+public class SpringCrudAPI {
 
 	public static void main(String[] args) {
-		SpringApplication.run(New2ApiApplication.class, args);
+		SpringApplication.run(SpringCrudAPI.class, args);
 	}
 
 }
